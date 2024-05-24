47804cf490e9e846f7bb4bb3aa56aa28d6ce8abd
Meenakshi Ratnawat <90496119+Meenakshiratnawat@users.noreply.github.com>
2024-05-25 01:45:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const categoryRoutes = require("./routes/category.js");
 const productRoutes = require("./routes/product.js");
 const stripeRoutes = require("./routes/stripepayment.js")
 
-//db conncetion
-// mongodb+srv://mahisingh8561:<password>@cluster0.6v5jv1q.mongodb.net/?retryWrites=true&w=majority
-// "mongodb+srv://mahisingh8561:6hbQJ-3R-QY9eTB@cluster0.nxcwe.mongodb.net/googleclone?retryWrites=true&w=majority",
 
 
 mongoose
@@ -33,44 +30,11 @@ mongoose
     console.error("Connection error", e.message);
   });
 
-// const db = mongoose.connection;
 
-// module.exports = db;
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv:/
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-// }) 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   // Allow requests from any origin
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-
-//   // Allow specific HTTP methods
-//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-
-//   // Allow specific headers
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-//   );
-
-//   // Allow credentials such as cookies
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Handle preflight request
-//   if (req.method === "OPTIONS") {
-//     res.status(200).end();
-//     return;
-//   }
-
-//   // Continue to the next middleware
-//   next();
-// });
 
 //my routes
 app.use("/api", authRoutes);
